a633af9ab50f252375f8b4c035c7bbbf337d0680
JavierSR99 <javiolopepicanela@gmail.com>
2025-07-17 13:16:06 +0200

    create Swagger config and setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Dragon Ball API')
+    .setDescription('Documentación API en Nest.JS')
+    .build();
+
+  const document = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
